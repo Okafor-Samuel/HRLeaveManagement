a3b4fd8244c4f8c3fa8a13f2cfce84882ead5d2c
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-16 09:27:14 +0100

    Add project files.

---
+++ HRLeaveManagementDomain/LeaveRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagementDomain
+{
+    public class LeaveRequest
+    {
+        public int Id { get; set; }
+        public DateTime Startdate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string RequestComments { get; set; }
+        public DateTime DateActioned { get; set; }
+        public bool? Approved { get; set; }
+        public bool? Cancelled { get; set; }
+    }
+}
---
+++ HRLeaveManagementDomain/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagementDomain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
