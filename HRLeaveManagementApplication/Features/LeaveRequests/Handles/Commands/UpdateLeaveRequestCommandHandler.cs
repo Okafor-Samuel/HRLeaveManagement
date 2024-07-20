706efa29494f43d2d9bf903f37378d2413fd8f16
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-20 08:06:35 +0100

    Adding mediatR and CQRS

---
+++ HRLeaveManagementApplication/Features/LeaveRequests/Handles/Commands/UpdateLeaveRequestCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HRLeaveManagementApplication.Features.LeaveRequests.Requests.Commands;
+using HRLeaveManagementApplication.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagementApplication.Features.LeaveRequests.Handles.Commands
+{
+    internal class UpdateLeaveRequestCommandHandler : IRequestHandler<UpdateLeaveRequestCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(UpdateLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.Get(request.UpdateLeaveRequestDto.Id);
+            _mapper.Map(request.UpdateLeaveRequestDto, leaveRequest);
+            await _leaveRequestRepository.Update(leaveRequest);
+            return Unit.Value;
+        }
+    }
+}
---
+++ HRLeaveManagementApplication/Features/LeaveRequests/Requests/Commands/UpdateLeaveRequestCommand.cs
@@ -0,0 +1,13 @@
+﻿using HRLeaveManagementApplication.DTOs.LeaveRequest;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagementApplication.Features.LeaveRequests.Requests.Commands
+{
+    public class UpdateLeaveRequestCommand : IRequest<Unit>
+    {
+        public UpdateLeaveRequestDto UpdateLeaveRequestDto { get; set; }
+    }
+}
