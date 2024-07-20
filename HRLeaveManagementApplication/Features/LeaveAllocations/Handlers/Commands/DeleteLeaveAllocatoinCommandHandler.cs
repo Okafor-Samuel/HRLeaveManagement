77837068bac3cb6b97926a0769efe769945b651e
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-20 08:39:05 +0100

    Adding mediatR and CQRS

---
+++ HRLeaveManagementApplication/Features/LeaveAllocations/Handlers/Commands/DeleteLeaveAllocatoinCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HRLeaveManagementApplication.Features.LeaveAllocations.Requests.Commands;
+using HRLeaveManagementApplication.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagementApplication.Features.LeaveAllocations.Handlers.Commands
+{
+    public class DeleteLeaveAllocatoinCommandHandler : IRequestHandler<DeleteLeaveAllocationCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        public DeleteLeaveAllocatoinCommandHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository.Get(request.Id);
+            await _leaveAllocationRepository.Delete(leaveAllocation);
+            return Unit.Value;
+        }
+    }
+}
