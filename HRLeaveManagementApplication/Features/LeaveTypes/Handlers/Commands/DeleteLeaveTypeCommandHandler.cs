ae92745c96689deeddd563888431b81129d0d9be
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-20 08:31:35 +0100

    Adding mediatR and CQRS

---
+++ HRLeaveManagementApplication/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HRLeaveManagementApplication.Features.LeaveTypes.Requests.Commands;
+using HRLeaveManagementApplication.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagementApplication.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeComand>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveTypeComand request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+            await _leaveTypeRepository.Delete(leaveType);
+            return Unit.Value;
+        }
+    }
+}
---
+++ HRLeaveManagementApplication/Features/LeaveTypes/Requests/Commands/DeleteLeaveTypeComand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagementApplication.Features.LeaveTypes.Requests.Commands
+{
+    public class DeleteLeaveTypeComand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
