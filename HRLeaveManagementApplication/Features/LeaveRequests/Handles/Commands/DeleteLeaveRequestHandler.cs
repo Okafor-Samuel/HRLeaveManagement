7c7582693e62508dd4c5a01264712644bf5483cf
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-20 08:35:31 +0100

    Adding mediatR and CQRS

---
+++ HRLeaveManagementApplication/Features/LeaveRequests/Handles/Commands/DeleteLeaveRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HRLeaveManagementApplication.Features.LeaveRequests.Requests.Commands;
+using HRLeaveManagementApplication.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagementApplication.Features.LeaveRequests.Handles.Commands
+{
+    public class DeleteLeaveRequestHandler : IRequestHandler<DeleteLeaveRequestCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        public DeleteLeaveRequestHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.Get(request.Id);
+            await _leaveRequestRepository.Delete(leaveRequest);
+            return Unit.Value;
+        }
+    }
+}
---
+++ HRLeaveManagementApplication/Features/LeaveRequests/Requests/Commands/DeleteLeaveRequestCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagementApplication.Features.LeaveRequests.Requests.Commands
+{
+    public class DeleteLeaveRequestCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
