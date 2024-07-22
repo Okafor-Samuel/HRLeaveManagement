7ab07883fa4d50349c4763c5537cdfb403416506
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-22 10:37:37 +0100

    fluent validation

--- HRLeaveManagementApplication/Persistence/Contracts/IGenericRepository.cs
+++ HRLeaveManagementApplication/Persistence/Contracts/IGenericRepository.cs
@@ -10,6 +10,7 @@ namespace HRLeaveManagementApplication.Persistence.Contracts
         Task<T> Get(int id);
         Task<IReadOnlyList<T>> GetAll();
         Task<T> Add(T entity);
+        Task<bool> Exists(int id);
         Task<T> Update(T entity);
         Task<T> Delete(T entity);
     }
