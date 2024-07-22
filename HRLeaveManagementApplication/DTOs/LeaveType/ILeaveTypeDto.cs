7853c2ff1fb4a37f743449c2e82e63a526741e87
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-22 10:39:19 +0100

    fluent validation

--- HRLeaveManagementApplication/DTOs/LeaveType/ILeaveTypeDto.cs
+++ HRLeaveManagementApplication/DTOs/LeaveType/ILeaveTypeDto.cs
@@ -1,11 +1,10 @@
-﻿using HRLeaveManagementApplication.DTOs.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HRLeaveManagementApplication.DTOs.LeaveType
 {
-    public class CreateLeaveTypeDto : ILeaveTypeDto
+    public interface ILeaveTypeDto
     {
         public string Name { get; set; }
         public int DefaultDays { get; set; }
--- HRLeaveManagementApplication/DTOs/LeaveType/LeaveTypeDto.cs
+++ HRLeaveManagementApplication/DTOs/LeaveType/LeaveTypeDto.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace HRLeaveManagementApplication.DTOs.LeaveType
 {
-    public class LeaveTypeDto : BaseDto
+    public class LeaveTypeDto : BaseDto, ILeaveTypeDto
     {
         public string Name { get; set; }
         public int DefaultDays { get; set; }
