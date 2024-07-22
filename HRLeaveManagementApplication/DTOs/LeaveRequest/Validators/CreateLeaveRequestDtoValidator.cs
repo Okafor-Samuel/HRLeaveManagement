310b2b25bb0c2bb5022620f4cdc54a83e58cd25c
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-22 10:38:24 +0100

    fluent validation

---
+++ HRLeaveManagementApplication/DTOs/LeaveRequest/Validators/CreateLeaveRequestDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using HRLeaveManagementApplication.Persistence.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagementApplication.DTOs.LeaveRequest.Validators
+{
+    public class CreateLeaveRequestDtoValidator : AbstractValidator<CreateLeaveRequestDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        public CreateLeaveRequestDtoValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+           _leaveTypeRepository = leaveTypeRepository;
+            Include(new ILeaveRequestDtoValidator(_leaveTypeRepository));
+        }
+    }
+}
---
+++ HRLeaveManagementApplication/DTOs/LeaveRequest/Validators/UpdateLeaveRequestDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using HRLeaveManagementApplication.Persistence.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagementApplication.DTOs.LeaveRequest.Validators
+{
+    public class UpdateLeaveRequestDtoValidator : AbstractValidator<UpdateLeaveRequestDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        public UpdateLeaveRequestDtoValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            Include(new ILeaveRequestDtoValidator(_leaveTypeRepository));
+
+            RuleFor(p => p.Id).NotNull().WithMessage("{PropertyName} must be present");
+        }
+    }
+}
