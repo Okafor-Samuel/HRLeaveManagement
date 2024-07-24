6c92e6dd3cfc044db8cf177cb20f592090591168
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-24 16:03:52 +0100

    repository pattern

---
+++ HRLeaveManagementPersistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using HRLeaveManagementApplication.Persistence.Contracts;
+using HRLeaveManagementPersistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagementPersistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDbContext>(Options =>
+            Options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+    }
+}
