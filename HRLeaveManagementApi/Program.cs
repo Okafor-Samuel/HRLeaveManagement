8212123c18b9e610725d49aa8e8aaa5049e7c12b
Samuel Okafor <okaforsamuel1000@gmail.com>
2024-07-27 10:35:09 +0100

    data seeding

--- HRLeaveManagementApi/Program.cs
+++ HRLeaveManagementApi/Program.cs
@@ -2,6 +2,7 @@
 using HRLeaveManagementApplication;
 using HRLeaveManagementInfrastructure;
 using HRLeaveManagementPersistence;
+using Microsoft.OpenApi.Models;
 
 namespace HRLeaveManagementApi
 {
@@ -19,7 +20,34 @@ namespace HRLeaveManagementApi
             builder.Services.ConfigureApplicationServices();
             builder.Services.ConfigureInfrastructureServices(builder.Configuration);
             builder.Services.ConfigurePersistenceServices(builder.Configuration);
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "HR Leave Management API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+            });
 
             builder.Services.AddCors(o =>
             {
